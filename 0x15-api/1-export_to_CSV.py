e8f8b2919a520cdffb3495e276cc608d8a05600b
Hammed Amusat Oriyomi <mederhoo@gmail.com>
2024-02-20 03:18:58 +0100

    api to csv

---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,75 @@
+#!/usr/bin/python3
+''' A Python script that, using this REST API,
+for a given employee ID, returns information
+about his/her TODO list progress.'''
+
+import requests
+import sys
+import csv
+
+def fetch_user_info(emp_id):
+    """fetch user data
+
+Args:
+    emp_id (int): employee id
+
+Returns:
+    user data: dict
+"""
+
+    users_url = f"https://jsonplaceholder.typicode.com/users/{emp_id}"
+    response = requests.get(users_url)
+    if response.status_code == 200:
+        user_data = response.json()
+        return user_data
+    else:
+        print(f"Failed to fetch user information for employee ID {emp_id}")
+        sys.exit(1)
+
+
+def fetch_todo_list(emp_id):
+    """_summary_
+    Args:
+        emp_id (_type_): _description_
+    Returns:
+    emp_id (int)
+    """
+    todos_url = f"https://jsonplaceholder.typicode.com/todos?userId={emp_id}"
+    response = requests.get(todos_url)
+    if response.status_code == 200:
+        todos_data = response.json()
+        return todos_data
+    else:
+        print(f"Failed to fetch TODO list for employee ID {emp_id}")
+        sys.exit(1)
+
+
+def main():
+    """main
+    """
+    if len(sys.argv) != 2 or not sys.argv[1].isdigit():
+        print("Usage: python3 script.py <employee_id>")
+        sys.exit(1)
+
+    emp_id = int(sys.argv[1])
+
+    user_info = fetch_user_info(emp_id)
+    todos = fetch_todo_list(emp_id)
+
+    csv_file_name = f"{emp_id}.csv"
+    with open(csv_file_name, 'w', newline='') as csvfile:
+        fieldnames = ['USER_ID', 'USERNAME', 'TASK_COMPLETED_STATUS', 'TASK_TITLE']
+        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+        
+        writer.writeheader()
+        for todo in todos:
+            writer.writerow({
+                'USER_ID': emp_id,
+                'USERNAME': user_info['name'],
+                'TASK_COMPLETED_STATUS': todo['completed'],
+                'TASK_TITLE': todo['title']
+            })
+
+
+if __name__ == "__main__":
+    main()
