e551f3238021b2c53a07f8516a69624f3fbf94fc
Hammed Amusat Oriyomi <mederhoo@gmail.com>
2024-03-07 01:38:08 +0100

    api

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -10,7 +10,7 @@ import requests
 def number_of_subscribers(subreddit):
     """get number of subscibers"""
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    response = requests.get(url)
+    response = requests.get(url, allow_redirects=False)
     if response.status_code == 404:
         return 0
     if response.status_code == 200:
