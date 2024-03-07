a26551c699a0e5af000f95c798990078ce49f472
Hammed Amusat Oriyomi <mederhoo@gmail.com>
2024-03-07 01:27:11 +0100

    api

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""a function that queries the Reddit API and returns the number of subscribers
+(not active users, total subscribers)
+for a given subreddit.
+If an invalid subreddit is given,
+the function should return 0."""
+import requests
+
+
+def number_of_subscribers(subreddit):
+	"""get number of subscibers"""
+	url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+	response = requests.get(url)
+	if response.status_code == 404:
+		return 0
+	if response.status_code == 200:
+		data = response.json()
+		return data.get("data").get("subscribers")
