ffc8c024ebbe0b15e26ad405c44d6d3a5d23ab8e
Hammed Amusat Oriyomi <mederhoo@gmail.com>
2024-03-07 11:27:06 +0100

    api

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,23 +1,27 @@
 #!/usr/bin/python3
-"""function that queries the Reddit API and prints the titles of the
-first 10 hot posts listed for a given subreddit."""
+"""
+Function that queries the Reddit API and prints the titles
+of the first 10 hot posts listed for a given subreddit.
+"""
 
 import requests
 
 
 def top_ten(subreddit):
-    url = f"https://www.reddit.com/r/{subreddit}/hot.json"
-    params = {"limit": 10}
-    headers = {'User-Agent': 'Mozilla/5.0'}
-    response = requests.get(url, headers=headers, params=params)
+    """
+    Function that queries the Reddit API
+    - If not a valid subreddit, print None.
+    """
+    req = requests.get(
+        "https://www.reddit.com/r/{}/hot.json".format(subreddit),
+        headers={"User-Agent": "Custom"},
+        params={"limit": 10},
+    )
 
-    # Check if the response is successful
-    if response.status_code == 200:
-        # Extract titles from the JSON response
-        data = response.json()
-        posts = data['data']['children']
-        for post in posts:
-            print(post['data']['title'])
+    if req.status_code == 200:
+        for get_data in req.json().get("data").get("children"):
+            dat = get_data.get("data")
+            title = dat.get("title")
+            print(title)
     else:
-        # Print None if the subreddit is invalid or there is an error
         print(None)
