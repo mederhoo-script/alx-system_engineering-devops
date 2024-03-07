5b5474375990284302df9829ff1d730da87464a8
Hammed Amusat Oriyomi <mederhoo@gmail.com>
2024-03-07 01:47:25 +0100

    api

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -16,8 +16,5 @@ def number_of_subscribers(subreddit):
     response = requests.get(url, headers=headers, allow_redirects=False)
     if response.status_code == 404:
         return 0
-    if response.status_code == 200:
-        data = response.json()
-        return data.get("data").get("subscribers")
-    else:
-        return 0
+    data = response.json()
+    return data.get("data").get("subscribers")
