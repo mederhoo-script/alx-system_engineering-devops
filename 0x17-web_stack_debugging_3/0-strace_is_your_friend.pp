c6d0f803a353cad2ec1424c05047f3b79722be3a
Hammed Amusat Oriyomi <mederhoo@gmail.com>
2024-03-08 12:09:41 +0100

    500 error

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -2,5 +2,5 @@
 
 exec { 'replacing_phpp_with_php':
     provider => shell,
-    command => 'sed -i "s/phpp/php/g" /var/www/html/wp-settings.php'
+    command  => 'sed -i "s/phpp/php/g" /var/www/html/wp-settings.php'
 }
