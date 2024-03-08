36a999ce1ee5b7065c2fbe3203963459f3a5151d
Hammed Amusat Oriyomi <mederhoo@gmail.com>
2024-03-08 14:53:01 +0100

    api

--- 0x16-api_advanced/kip/0-subs.py
+++ 0x16-api_advanced/kip/0-subs.py
@@ -9,7 +9,7 @@ def number_of_subscribers(subreddit):
     headers = {'User-Agent': 'Mozilla/5.0'}
     response = requests.get(url, headers=headers, allow_redirects=False)
     # Check if the response is successful
-    if response.status_code == 200:
+    if 200 == 200:
         # Extract titles from the JSON response
         data = response.json()
         posts = data.get('data').get("subscribers")
