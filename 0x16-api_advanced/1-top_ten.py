f9a63d65539bdabc3658fc7eafba8d6b171adc9d
Hammed Amusat Oriyomi <mederhoo@gmail.com>
2024-03-08 13:44:25 +0100

    apii

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -2,23 +2,26 @@
 """Function to print hot posts on a given Reddit subreddit."""
 import requests
 import sys
+import json
 
 
-def top_tens(subreddit):
+def top_ten(subreddit):
+    """api"""
     url = f"https://www.reddit.com/r/{subreddit}/hot/.json"
     headers = {"User-Agent": "linux"}
     params = {"limit": 10}
-    response = requests.get(url, headers=headers, params=params, allow_redirects=False)
-    
+    response = requests.get(url, headers=headers, params=params,
+                            allow_redirects=False)
+
     if response.status_code == 404:
         print("Subreddit not found")
         return
     elif response.status_code != 200:
         print("Failed to retrieve data from Reddit API")
         return
-    
+
     try:
-        data = response.json()
+        data = json.loads(response.text)  # Parse the response content manually
         if 'data' in data and 'children' in data['data']:
             posts = data['data']['children']
             for post in posts:
@@ -28,6 +31,7 @@ def top_tens(subreddit):
             print("No posts found")
     except Exception as e:
         print("Error decoding JSON response:", e)
+
 
 if __name__ == "__main__":
     if len(sys.argv) != 2:
