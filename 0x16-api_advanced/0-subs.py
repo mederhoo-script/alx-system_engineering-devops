d5ea7e2a737053b8d8092a50a32410f330ec256f
Hammed Amusat Oriyomi <mederhoo@gmail.com>
2024-03-13 14:49:31 +0100

    api

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -5,7 +5,8 @@ import requests
 
 def number_of_subscribers(subreddit):
     """Return the total number of subscribers on a given subreddit."""
-    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    urls = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    url = urls.strip()
     headers = {
         "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
     }
